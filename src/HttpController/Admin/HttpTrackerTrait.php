06bf54adc94f9c44b5c1660540d5fda615e862df
xiezhouhuang <564643122@qq.com>
2022-07-16 18:37:51 +0800

    # 修复bug

--- src/HttpController/Admin/HttpTrackerTrait.php
+++ src/HttpController/Admin/HttpTrackerTrait.php
@@ -8,7 +8,7 @@ use Kyzone\EsUtility\Common\Exception\HttpParamException;
 use Kyzone\EsUtility\Common\Http\Code;
 
 /**
- * @property \App\Model\Log\HttpTracker $Model
+ * @property \App\Model\HttpTracker $Model
  */
 trait HttpTrackerTrait
 {
@@ -30,59 +30,6 @@ trait HttpTrackerTrait
 			$this->Model->where($this->get['where']);
 		}
 		return null;
-
-		// 已废弃
-		return function (QueryBuilder $builder) {
-			$filter = $this->filter();
-			$builder->where('instime', [$filter['begintime'], $filter['endtime']], 'between');
-			if (isset($filter['repeated']) && $filter['repeated'] !== '') {
-				$builder->where('repeated', $filter['repeated']);
-			}
-
-			// envkey: {"one":"point_name","two":"point_id"}
-			// envvalue: {"one":"123","two":"4556"}
-			foreach (['envkey', 'envvalue'] as $col) {
-				if ( ! empty($filter[$col])) {
-					$filter[$col] = json_decode($filter[$col], true);
-				}
-			}
-
-			if ( ! empty($filter['envkey'])) {
-				foreach ($filter['envkey'] as $key => $value) {
-					if ($like = $filter['envvalue'][$key]) {
-						$calc = true;
-						// 支持逻辑运算转换为like
-						$symbol = ['&&' => ' AND ', '||' => ' OR '];
-						foreach ($symbol as $sym => $join) {
-							if (strpos($like, $sym) !== false) {
-								$tmp = [];
-								$arr = explode($sym, $like);
-								foreach ($arr as $item) {
-									$item && $tmp[] = "$value LIKE '%{$item}%'";
-								}
-								if ($tmp) {
-									$tmp = implode($join, $tmp);
-									$builder->where("($tmp)");
-									$calc = false;
-								}
-							}
-						}
-						if ($calc) {
-							$builder->where($value, "%{$like}%", 'LIKE');
-						}
-					}
-				}
-			}
-
-			$runtime = $filter['runtime'] ?? 0;
-			if ($runtime > 0) {
-				$builder->where('runtime', $runtime, '>=');
-			}
-			/*
-			 * 生成的SQL分析示例
-			 * explain partitions SELECT SQL_CALC_FOUND_ROWS * FROM `http_tracker` WHERE  `instime` between 1646197200 AND 1647493199  AND `point_name` LIKE '%123%'  AND (point_id LIKE '%4556%' AND point_id LIKE '%789%') ORDER BY instime DESC  LIMIT 0, 100\G
-			 * */
-		};
 	}
 
 	// 单条复发
@@ -150,7 +97,7 @@ trait HttpTrackerTrait
 				trace('HttpTracker 开始 ');
 
 				/** @var AbstractModel $model */
-				$model = model_log('HttpTracker');
+				$model = model('HttpTracker');
 				$model->where($where)->chunk(function ($item) {
 					$item->repeatOne();
 				}, 300);
